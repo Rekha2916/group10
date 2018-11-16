93ef678aba746ba31016ae2e9d51d2a84a7e915b
Rekha2916 <rekhakumawat19@gmail.com>
2018-11-16 15:21:30 +0530

    Initial commit

---
+++ src/main/java/com/controller/UsernameDisplay.java
@@ -0,0 +1,44 @@
+package com.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class UsernameDisplay
+ */
+public class UsernameDisplay extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UsernameDisplay() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String s1 = request.getParameter("uname");
+		PrintWriter pw = response.getWriter();
+		pw.println("Tumcha naav "+s1+" aahe");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,12 @@
+<html>
+<head>Group 10(excluding Manoj)</head>
+<body>
+<h2>Hello World!</h2>
+<form action="UsernameDisplay">
+	<label for="uname">Username Daal</label>
+	<input type="text" name="uname">
+	<input type="submit" value="Daaba">
+	<h1>Kahich nai honar</h1>
+</form>
+</body>
+</html>
